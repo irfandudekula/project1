8a599289394b8dd2047a55e5fb70506cf2b36eea
irfandudekula <59482806+irfandudekula@users.noreply.github.com>
2020-07-09 12:37:41 +0530

    Add files via upload

---
+++ Scripts/OptionsScript.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class OptionsScript : MonoBehaviour
+{
+    public GameObject Opt,OptUp,OptDown;
+    int s = 0,i;
+    private void Start()
+    {
+        Options();
+    }
+
+    public void Options()
+    {
+        Vector3 res = Opt.transform.localScale;
+        if (s==0)
+        {
+            while (res.z >0f)
+            {
+                res.z -= 0.1f;
+                Opt.transform.localScale = res;
+            }
+            res.z = 0f;
+            Opt.transform.localScale = res;
+            s = 1;
+            OptUp.SetActive(true);
+            OptDown.SetActive(false);
+        }
+        else
+        {        
+            while (res.z < 4f)
+            {
+                
+                res.z += 0.1f;
+                Opt.transform.localScale = res;
+            }
+            res.z = 4f;
+            Opt.transform.localScale = res;
+            s = 0;
+            OptUp.SetActive(false);
+            OptDown.SetActive(true);
+        }
+    }
+   
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+
+    
+
+}
---
+++ Scripts/SlidesScript.cs
@@ -0,0 +1,195 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlidesScript : MonoBehaviour
+{
+    public GameObject nextBtn;
+    public GameObject prevBtn;
+    public GameObject VideoBtn, SlideBtn, ExpBtn, firstSlide;
+    public GameObject[] slides;
+    public GameObject[] ExpData;
+    public GameObject[] Videos;
+    public GameObject[] VidPlayButtons;
+    private int counter = 0, s1 = 0, v1 = 0, e1 = 0;
+
+    void Start()
+    {
+        StopVideo();
+        foreach (GameObject s in slides)
+            s.SetActive(false);
+        foreach (GameObject e in ExpData)
+            e.SetActive(false);
+        StopVideo();
+        prevBtn.SetActive(false);
+        nextBtn.SetActive(false);
+        firstSlide.SetActive(true);
+       
+    }
+
+    public void Experiment()
+    {
+        s1 = 0;
+        v1 = 0;
+        e1 = 1;
+        firstSlide.SetActive(false);
+        SlideBtn.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        VideoBtn.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        ExpBtn.GetComponent<RectTransform>().localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        StopVideo();
+        foreach (GameObject s in slides)
+            s.SetActive(false);
+        nextBtn.SetActive(true);
+        counter = -1;
+        next();
+    }
+
+    public void slide()
+    {
+        s1 = 1;
+        v1 = 0;
+        e1 = 0;
+        StopVideo();
+        firstSlide.SetActive(false);
+        foreach (GameObject e in ExpData)
+            e.SetActive(false);
+        SlideBtn.GetComponent<RectTransform>().localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        VideoBtn.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        ExpBtn.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        nextBtn.SetActive(true);
+        counter = -1;
+        next();
+    }
+
+    public void video()
+    {
+        v1 = 1;
+        s1 = 0;
+        e1 = 0;
+        foreach (GameObject s in slides)
+            s.SetActive(false);
+        foreach (GameObject e in ExpData)
+            e.SetActive(false);
+        StopVideo();
+        firstSlide.SetActive(false);
+        VideoBtn.GetComponent<RectTransform>().localScale = new Vector3(0.8f, 0.8f, 0.8f);
+        SlideBtn.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        ExpBtn.GetComponent<RectTransform>().localScale = new Vector3(1f, 1f, 1f);
+        nextBtn.SetActive(true);
+        counter = -1;
+        next();
+    }
+
+    public void next()
+    {
+        if (s1 == 1)
+        {
+            checkNxt(slides.Length);
+            DisplayDataIn(slides);           
+        }
+
+        if (v1 == 1)
+        {
+            checkNxt(Videos.Length);
+            DisplayVideoData();
+        }
+
+        if (e1 == 1)
+        {
+            checkNxt(ExpData.Length);
+            DisplayDataIn(ExpData);
+        }
+    }
+
+    public void prev()
+    {
+        if (s1 == 1)
+        {
+            checkPrev(slides.Length);
+            DisplayDataIn(slides);       
+        }
+
+        if (v1 == 1)
+        {
+            checkPrev(Videos.Length);
+            DisplayVideoData();
+        }
+
+        if (e1 == 1)
+        {
+            checkPrev(ExpData.Length);
+            DisplayDataIn(ExpData);      
+        }
+    }
+
+    private void checkNxt(int size)
+    {
+        counter++;
+        if (counter >= size)
+            counter -= 1;
+        checkBtns(size);
+    }
+
+    private void checkPrev(int size)
+    {
+        counter--;
+        if (counter < 0)
+            counter = 0;
+        checkBtns(size);
+    }
+
+    private void checkBtns(int len)
+    {
+    if (counter <= 0)
+        prevBtn.SetActive(false);
+    else
+        prevBtn.SetActive(true);
+
+    if (counter >= len - 1)
+        nextBtn.SetActive(false);
+    else
+        nextBtn.SetActive(true);
+    }
+
+    private void StopVideo()
+    {
+        foreach (GameObject VidPlayBtn in VidPlayButtons)
+            VidPlayBtn.SetActive(false);
+        foreach (GameObject v in Videos)
+            v.SetActive(false);
+        Videos[0].SetActive(true);
+        VidPlayButtons[0].SetActive(false);
+
+    }
+
+    private void DisplayDataIn(GameObject[] data)
+    {
+        foreach (GameObject d in data)
+        {
+            if (d == data[counter])
+                d.SetActive(true);
+            else
+                d.SetActive(false);
+        }
+    }
+
+    private void DisplayVideoData()
+    {
+        foreach (GameObject VidPlayBtn in VidPlayButtons)
+        {
+            VidPlayBtn.SetActive(false);
+        }
+        foreach (GameObject v in Videos)
+        {
+            if (v == Videos[counter])
+            {
+                v.SetActive(true);
+                VidPlayButtons[counter].SetActive(true);
+            }
+            else
+                v.SetActive(false);
+        }
+    }
+
+}
